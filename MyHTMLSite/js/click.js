87704a35a2989895246f503a39d0400a1df9b7a8
Janeal Sanchez <jasanche@jasanche-mn1.linkedin.biz>
2018-06-28 00:04:53 -0700

    index.html

---
+++ MyHTMLSite/js/click.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    let clicks = 0;  
+    const div1 = document.getElementById('div1');
+    div1.addEventListener('click', function() {
+        clicks++;
+        div1.innerHTML = 'Clicked! ' + clicks + ' times!';
+        if (clicks >= 10){
+            div1.innerHTML = 'Clicked! ' + clicks + ' wow!';
+        }
+        if (clicks >= 50){
+            div1.innerHTML = 'STOP! ' + clicks + ' you need help!';
+        }
+    });  
+})
+ 

1ef028aa840612a0717a57e631d0c1514e31e2b7
Janeal Sanchez <jasanche@jasanche-mn1.linkedin.biz>
2018-07-23 08:47:16 -0700

    fixes

--- 4.1_Python_Lab.py
+++ 4.1_Python_Lab.py
@@ -16,23 +16,48 @@ Time: '9:09'    Output: 'unknown'
 def main():
   # Construct a dictionary that will contain the necessary number to string
   # mappings for you to do translations.
-  time_mappings = # YOUR CODE HERE
-
+  minute_mappings ={"00":" ", "10": " ten", "15":" fifteen", "30": " thirty", "45":" forty-five"} 
+  hour_mapping={"1":" one","2":" two","3":" three","4":" four","5":" five","6":" six","7":" seven"}# YOUR CODE HERE
+  # print(minute_mappings)
   # Load in this JSON data using the `loads` function from the imported `json` library
   appointments_data = '{"monday": "3:45", "tuesday": "4:00", "wednesday": "2:15", "thursday": "5:30", "friday": "7:23", "weekends": "1:10"}'
-  appointments_dict = # YOUR CODE HERE
-
+  appointments_dict = json.loads(appointments_data)
+  # print(appointments_dict)
   # Extract all the times from the loaded data and store them in a list
   # For each time, you will need to separate it into hours (left of the :) and minutes (right of the :)
   # Hint: Lookup the `split` function for strings in Python
   #
   # YOUR CODE HERE
-  #
+  appointments_dict.values()
+  num = appointments_dict.values()
+  # print(num)
+  time_output_string = ""
+  for x in num:
+    # print(x)
+    bro = x.split(":")
+    minutes = bro[1]
+    hours = bro[0]
+    if minutes in minute_mappings.keys(): 
+      if hours in hour_mapping.keys(): 
+        minutes_in_word = minute_mappings[minutes]
+        hours_in_word = hour_mapping[hours]
+        time_output_string += "Time:" + x  +  "   Output:" + hours_in_word + minutes_in_word + "\n"
+    else:
+        time_output_string += "Time" + x  +  "   Output: unknown" + "\n"  
+  return time_output_string
+    
+    
+     
+   
+
+   
+  # print(num.split(""))
 
   # Return the list of results
   #
   # YOUR CODE HERE
-  #
+
+  
 
 """
 The `if __name__ == "__main__":` structure is commonly seen in Python files.
--- MyHTMLSite/js/working.js
+++ MyHTMLSite/js/working.js
@@ -1,72 +1,94 @@
-window.onload = function() {  
+window.onload = function () {
     function processForm(e) {
         e.preventDefault();
-
-        var data = $("#city option:selected").text();
-        var temp = {};
-       temp["weather"] = data; // New York, NY
-     // lower case
-       var str = data.split(', ');
-       
-       var city = str[0].toLowerCase();
-       var state = str[1].toLowerCase();
-        console.log(city);
-        console.log(state);
-       
-        var empty ="";
-       empty +="https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22";
-       empty +=city;
-       empty +="%2C%20";
-       empty +=state;
-       empty +="%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-       $.get(empty, function(data){
-        console.log(data.query.results.channel.item.condition.text);
         
 
-        document.getElementById("helo").innerHTML = data.query.results.channel.item.condition.text; 
-        var weather= data.query.results.channel.item.condition.text;
-        var result=$("#img");
-        if(weather === "Partly Cloudy"){
-            $("#body").css("background-color", "#FCFEFC");
-            $("#helo").css("color", "black");
-                        
-        }
-        else if(weather === "Sunny"){
-            $("#body").css("background-color", "lightyellow");
-        }
-        else{
-            $("#body").css("background-color", "black");
-        }
-       }); 
-        }
-     var form = $("#my-forma");  
-     form.submit( processForm);
+        var data = $("#city option:selected").text();
+        var temp = {};
+        temp["weather"] = data; // New York, NY
+        // lower case
+        var str = data.split(', ');
+        var city = str[0].toLowerCase();
+        var state = str[1].toLowerCase();
+        console.log(city);
+        console.log(state);
+        
+        var empty = "";
+        empty += "https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22";
+        empty += city;
+        empty += "%2C%20";
+        empty += state;
+        empty += "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+        $.get(empty, function (data) {
+            console.log(data.query.results.channel.item.condition.text);
+            console.log(data.query.results.channel.item.condition);
+            document.getElementById("wat").innerHTML = data.query.results.channel.item.condition.date;
+            document.getElementById("yell").innerHTML ="Temp: " + data.query.results.channel.item.condition.temp;
+            document.getElementById("helo").innerHTML = data.query.results.channel.item.condition.text;
+            var weather = data.query.results.channel.item.condition.text;
+            
+            if (weather === "Partly Cloudy") {
+                $("#body").css("background-color", "#E5E8E8");
+              
+                
+            }
+            else if (weather === "Sunny") {
+                $("#body").css("background-color", "lightyellow");
+               
+                
+            }
+            else if (weather === "Windy") {
+                $("#body").css("background-color", "#95A5A6");
+            
+            }
+            else if (weather === "Mostly Sunny") {
+                $("#body").css("background-color", " #FAD7A0");
+            
+            }
+            else if (weather === "Cloudy") {
+                $("#body").css("background-color", "#76D7C4");
+               
+            }
+            else if (weather === "Mostly Cloudy") {
+                $("#body").css("background-color", "#D2B4DE");
+              
+            }
+            else {
+                $("#body").css("background-color", "#85C1E9");
+               
+                
+            }
+        });
+    }
+    var form = $("#my-forma");
+    form.submit(processForm);
 
-///transition
-var counter = 0, // to keep track of current slide
-    $items = $('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
+    //transition
+    var counter = 0, // to keep track of current slide
+        $items = $('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+        numItems = $items.length; // total number of slides
 
-// this function is what cycles the slides, showing the next or previous slide and hiding all the others
-var showCurrent = function(){
-    var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
-   
-  $items.removeClass('show'); // remove .show from whichever element currently has it
-  $items.eq(itemToShow).addClass('show');    
-};
+    // this function is what cycles the slides, showing the next or previous slide and hiding all the others
+    var showCurrent = function () {
+        var itemToShow = Math.abs(counter % numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
 
-// add click events to prev & next buttons 
-$('.next').on('click', function(){
-    counter++;
-    showCurrent(); 
-});
-$('.prev').on('click', function(){
-    counter--;
-    showCurrent(); 
-});
+        $items.removeClass('show'); // remove .show from whichever element currently has it
+        $items.eq(itemToShow).addClass('show');
+    };
+    //hover
 
-window.setInterval(function () {
-    counter++;
-      showCurrent(); // increment & show
-  }, 4000);
- }
+    // add click events to prev & next buttons 
+    $('.next').on('click', function () {
+        counter++;
+        showCurrent();
+    });
+    $('.prev').on('click', function () {
+        counter--;
+        showCurrent();
+    });
+
+    window.setInterval(function () {
+        counter++;
+        showCurrent(); // increment & show
+    }, 4000);
+}
